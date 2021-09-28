1864c4b74990094e4f65c820d3e59783cafd6037
kam-in <52216983+kam-in@users.noreply.github.com>
2021-09-28 15:01:56 -0700

    maze theme are defined in Maze_theme.py and imported to Maze_main.py

---
+++ Maze_theme.py
@@ -0,0 +1,33 @@
+# prepare themes
+maze_theme = {
+    'face' : ['happyface', 'original_1', 'milkywhite'],
+    'dolphin' : ['dolphin', 'ocean_1', 'ocean_blue2'],
+    'monkey' : ['monkey', 'jungle_1', 'plain_green'],
+    'bird' : ['bird', 'sky_1', 'white'],
+    'cow' : ['cow', 'pasture_1', 'grassgreen'],
+    'face': ['happyface', 'original_1', 'milkywhite'],
+    'dolphin': ['dolphin', 'ocean_1', 'ocean_blue2'],
+    'monkey': ['monkey', 'jungle_1', 'plain_green'],
+    'bird': ['bird', 'sky_1', 'white'],
+    'cow': ['cow', 'pasture_1', 'grassgreen'],
+    'face': ['happyface', 'original_1', 'milkywhite'],
+    'dolphin': ['dolphin', 'ocean_1', 'ocean_blue2'],
+    'monkey': ['monkey', 'jungle_1', 'plain_green'],
+    'bird': ['bird', 'sky_1', 'white'],
+    'cow': ['cow', 'pasture_1', 'grassgreen'],
+    'face': ['happyface', 'original_1', 'milkywhite'],
+    'dolphin': ['dolphin', 'ocean_1', 'ocean_blue2'],
+    'monkey': ['monkey', 'jungle_1', 'plain_green'],
+    'bird': ['bird', 'sky_1', 'white'],
+    'cow': ['cow', 'pasture_1', 'grassgreen'],
+    'face': ['happyface', 'original_1', 'milkywhite'],
+    'dolphin': ['dolphin', 'ocean_1', 'ocean_blue2'],
+    'monkey': ['monkey', 'jungle_1', 'plain_green'],
+    'bird': ['bird', 'sky_1', 'white']
+}
+
+theme_strings = ['face', 'dolphin', 'monkey', 'bird', 'cow',
+                 'face', 'dolphin', 'monkey', 'bird', 'cow',
+                 'face', 'dolphin', 'monkey', 'bird', 'cow',
+                 'face', 'dolphin', 'monkey', 'bird', 'cow',
+                 'face', 'dolphin', 'monkey', 'bird']
