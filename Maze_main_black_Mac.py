18dc4911dc6446f211e2b5ca4644077a9dfe9451
kam-in <52216983+kam-in@users.noreply.github.com>
2022-01-14 11:41:34 -0800

    key input clearing - accepted with comments

--- Maze_main_black_Mac.py
+++ Maze_main_black_Mac.py
@@ -60,8 +60,7 @@ def run_trial(display, screen, trial_map, spr_player, spr_tiles, background, blo
         def __init__(self, x, y):
             self.x = x
             self.y = y 
-            self.sides = [1,1,1,1]
-        
+
         def cal_4_sides(self):
             sides = [1,1,1,1]
             right_index = ((self.x+32) / 32, self.y / 32)
@@ -141,8 +140,8 @@ def run_trial(display, screen, trial_map, spr_player, spr_tiles, background, blo
                 block = 'Z' # deadend
             if 2 in sides:
                 block = 'G' #  target revealed
-
-            # needs to be defined: E (entering)
+            if (self.x // 32, self.y // 32) == target:
+                block = 'E'  # entering/ goal location
 
             return block
 
@@ -237,6 +236,7 @@ def run_trial(display, screen, trial_map, spr_player, spr_tiles, background, blo
             # this blits the tiles at the position, but starting with 6*32 end ending 32 further
             screen.blit(spr_tiles, (int(self.x), int(self.y)), (self.type * 32, 0, 32, 32))
 
+    pygame.event.clear() # clear events when initializing a maze
     load = []
     remove = []
     player = Player(0, 0) # initial x y coordinate for a player is always [0 0]
@@ -328,6 +328,7 @@ def run_guess(display, screen, trial_map, spr_tiles, background, block_num, tria
     pygame.display.flip()
 
     # For a valid click, give a visual feedback & log the response
+    pygame.event.clear()  # clear events first
     correctClick = False
     while not correctClick:
         events = pygame.event.get()
@@ -404,6 +405,7 @@ def display_message_timed(surface, text, text_color, holdtime):
     time.sleep(holdtime)
 
 def display_message_key(surface, text, text_color):
+    pygame.event.clear() # clear events first
     wait = True
     render_text(surface, text, 35, text_color)
     pygame.display.flip()
